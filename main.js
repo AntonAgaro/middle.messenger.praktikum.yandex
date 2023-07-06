c412ae9de229bbec25c5af073f30170c59ad2e70
Anton Agaronov <anton.agaronov@legalbet.com>
2023-07-06 23:31:20 +0400

    feat: add error page

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ import '@/scss/app.scss'
 import Handlebars from 'handlebars/runtime'
 import renderLoginPage from '@/pages/Login'
 import renderSignUp from '@/pages/SignUp/index.js'
-// Handlebars.registerPartial('form', form)
+import errorPageTmpl from '@/pages/ErrorPage/error.hbs'
+import routerLink from '@components/routerLink/routerLink.hbs'
+
+Handlebars.registerPartial('routerLink', routerLink)
+
+import { render } from '@/utils/functions.js'
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('app')
 
   // root.innerHTML = renderLoginPage()
-  root.innerHTML = renderSignUp()
+  // root.innerHTML = renderSignUp()
+  root.innerHTML = render(errorPageTmpl, { title: 404, subtitle: 'Не туда попали' })
 })
