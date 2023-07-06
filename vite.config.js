90c1cd386abcb85d79459ff8a177f9ba2568ed52
Anton Agaronov <anton.agaronov@legalbet.com>
2023-07-06 13:26:30 +0400

    feat: add Handlebars, app styles

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
+import handlebars from 'vite-plugin-handlebars'
+import { resolve } from 'path'
+import path from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 export default defineConfig({
+  publicDir: 'static',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  plugins: [
+    handlebars({
+      context: {},
+      partialDirectory: resolve(__dirname, './src/partials'),
+    }),
+  ],
 })
