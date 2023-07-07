96d5e3a3d8fe5dd507517fb316445699c834d05d
Anton Agaronov <anton.agaronov@legalbet.com>
2023-07-07 12:41:30 +0400

    feat: add chats list

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ import routerLink from '@components/routerLink/routerLink.hbs'
 
 import { render } from '@/utils/functions.js'
 import renderErrorPage from '@/pages/ErrorPage'
+import renderChatPage from '@/pages/Chats/index.js'
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('app')
 
   // root.innerHTML = renderLoginPage()
   // root.innerHTML = renderSignUp()
-  root.innerHTML = renderErrorPage(500)
+  // root.innerHTML = renderErrorPage(500)
+  root.innerHTML = renderChatPage()
 })
