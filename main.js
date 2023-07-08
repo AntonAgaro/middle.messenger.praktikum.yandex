1a9fa97768ecf62ada8787f830ecacb4ae1d0cc2
Anton Agaronov <anton.agaronov@legalbet.com>
2023-07-08 08:31:37 +0400

    feat: add user page

--- main.js
+++ main.js
@@ -8,11 +8,25 @@ import routerLink from '@components/routerLink/routerLink.hbs'
 import { render } from '@/utils/functions.js'
 import renderErrorPage from '@/pages/ErrorPage'
 import renderChatPage from '@/pages/Chats/index.js'
+import renderUserPage from '@/pages/User/index.js'
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('app')
 
   // root.innerHTML = renderLoginPage()
   // root.innerHTML = renderSignUp()
   // root.innerHTML = renderErrorPage(500)
-  root.innerHTML = renderChatPage()
+  // root.innerHTML = renderChatPage()
+  const userData = {
+    userLogo: '',
+    userName: 'Anton',
+    userDetails: [
+      { title: 'Почта', value: 'pochta@yandex.ru', inputName: 'email' },
+      { title: 'Логин', value: 'ivanivanov', inputName: 'login' },
+      { title: 'Имя', value: 'Ivan', inputName: 'first_name' },
+      { title: 'Фамилия', value: 'Ivanov', inputName: 'second_name' },
+      { title: 'Имя в чате', value: 'Ivan', inputName: 'display_name' },
+      { title: 'Телефон', value: '+7 (909) 967 30 30', inputName: 'phone' },
+    ],
+  }
+  root.innerHTML = renderUserPage(userData)
 })
