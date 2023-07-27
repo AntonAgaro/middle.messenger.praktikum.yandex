b053403dafd9e3faf6b15c0ae4229dd8a6ea8b2b
Anton Agaronov <anton.agaronov@legalbet.com>
2023-07-27 10:07:19 +0400

    add validation for signup page

--- src/pages/SignUp/index.ts
+++ src/pages/SignUp/index.ts
@@ -4,103 +4,161 @@ import SignUpPage from './SignUpPage'
 import { render } from '../../utils/functions'
 import Input from '../../components/input/Input'
 import InputGroup from '../../components/inputGroup/InputGroup'
+import Component from '../../classes/Component'
+import Validator from '../../classes/Validator'
 
 export default function renderSignUp() {
-  const emailInput = new InputGroup({
-    input: new Input({
-      attrs: {
-        class: 'input',
-        name: 'email',
-        type: 'text',
-        id: 'email',
-        value: '',
-        required: true,
+  let emailInputGroup: Component
+  let loginInputGroup: Component
+  let nameInputGroup: Component
+  let surnameInputGroup: Component
+  let phoneInputGroup: Component
+  let passInputGroup: Component
+  let repeatPassInputGroup: Component
+
+  const emailInput = new Input({
+    attrs: {
+      class: 'input',
+      name: 'email',
+      type: 'text',
+      id: 'email',
+      value: '',
+      required: true,
+    },
+    events: {
+      blur() {
+        Validator.validate(emailInput, Validator.checkEmail, emailInputGroup)
       },
-    }),
+    },
+  })
+  emailInputGroup = new InputGroup({
+    input: emailInput,
     name: 'email',
     label: 'Почта',
   })
-  const loginInput = new InputGroup({
-    input: new Input({
-      attrs: {
-        class: 'input',
-        name: 'login',
-        type: 'text',
-        id: 'login',
-        value: '',
-        required: true,
+
+  const loginInput = new Input({
+    attrs: {
+      class: 'input',
+      name: 'login',
+      type: 'text',
+      id: 'login',
+      value: '',
+      required: true,
+    },
+    events: {
+      blur() {
+        Validator.validate(loginInput, Validator.checkLogin, loginInputGroup)
       },
-    }),
+    },
+  })
+  loginInputGroup = new InputGroup({
+    input: loginInput,
     label: 'Логин',
     name: 'login',
   })
-  const nameInput = new InputGroup({
-    input: new Input({
-      attrs: {
-        class: 'input',
-        name: 'first_name',
-        type: 'text',
-        id: 'first_name',
-        value: '',
-        required: true,
+
+  const nameInput = new Input({
+    attrs: {
+      class: 'input',
+      name: 'first_name',
+      type: 'text',
+      id: 'first_name',
+      value: '',
+      required: true,
+    },
+    events: {
+      blur() {
+        Validator.validate(nameInput, Validator.checkNames, nameInputGroup)
       },
-    }),
+    },
+  })
+  nameInputGroup = new InputGroup({
+    input: nameInput,
     label: 'Имя',
     name: 'first_name',
   })
-  const surnameInput = new InputGroup({
-    input: new Input({
-      attrs: {
-        class: 'input',
-        name: 'second_name',
-        type: 'text',
-        id: 'second_name',
-        value: '',
-        required: true,
+
+  const surnameInput = new Input({
+    attrs: {
+      class: 'input',
+      name: 'second_name',
+      type: 'text',
+      id: 'second_name',
+      value: '',
+      required: true,
+    },
+    events: {
+      blur() {
+        Validator.validate(surnameInput, Validator.checkNames, surnameInputGroup)
       },
-    }),
+    },
+  })
+  surnameInputGroup = new InputGroup({
+    input: surnameInput,
     label: 'Фамилия',
     name: 'second_name',
   })
-  const phoneInput = new InputGroup({
-    input: new Input({
-      attrs: {
-        class: 'input',
-        name: 'phone',
-        type: 'text',
-        id: 'phone',
-        value: '',
-        required: true,
+
+  const phoneInput = new Input({
+    attrs: {
+      class: 'input',
+      name: 'phone',
+      type: 'text',
+      id: 'phone',
+      value: '',
+      required: true,
+    },
+    events: {
+      blur() {
+        Validator.validate(phoneInput, Validator.checkPhone, phoneInputGroup)
       },
-    }),
+    },
+  })
+  phoneInputGroup = new InputGroup({
+    input: phoneInput,
     label: 'Телефон',
     name: 'phone',
   })
-  const passInput = new InputGroup({
-    input: new Input({
-      attrs: {
-        class: 'input',
-        name: 'password',
-        type: 'password',
-        id: 'password',
-        value: '',
-        required: true,
+
+  const passInput = new Input({
+    attrs: {
+      class: 'input',
+      name: 'password',
+      type: 'password',
+      id: 'password',
+      value: '',
+      required: true,
+    },
+    events: {
+      blur() {
+        Validator.validate(passInput, Validator.checkPass, passInputGroup)
       },
-    }),
+    },
+  })
+  passInputGroup = new InputGroup({
+    input: passInput,
     label: 'Пароль',
     name: 'password',
   })
-  const repeatPassInput = new InputGroup({
-    input: new Input({
-      attrs: {
-        class: 'input',
-        name: 'repeat_password',
-        type: 'password',
-        id: 'repeat_password',
-        value: '',
-        required: true,
+
+  const repeatPassInput = new Input({
+    attrs: {
+      class: 'input',
+      name: 'repeat_password',
+      type: 'password',
+      id: 'repeat_password',
+      value: '',
+      required: true,
+    },
+    events: {
+      blur() {
+        Validator.validate(repeatPassInput, Validator.checkPass, repeatPassInputGroup)
       },
-    }),
+    },
+  })
+  repeatPassInputGroup = new InputGroup({
+    input: repeatPassInput,
     label: 'Пароль (еще раз)',
     name: 'repeat_password',
   })
@@ -108,6 +166,33 @@ export default function renderSignUp() {
     text: 'Зарегистрироваться',
     attrs: {
       class: 'button',
+    },
+    events: {
+      click: (e: Event) => {
+        e.preventDefault()
+        const target = e.target as HTMLElement
+        const form = target.closest('form')
+        const validationArray = [
+          Validator.validate(emailInput, Validator.checkEmail, emailInputGroup),
+          Validator.validate(loginInput, Validator.checkLogin, loginInputGroup),
+          Validator.validate(nameInput, Validator.checkNames, nameInputGroup),
+          Validator.validate(surnameInput, Validator.checkNames, surnameInputGroup),
+          Validator.validate(phoneInput, Validator.checkPhone, phoneInputGroup),
+          Validator.validate(passInput, Validator.checkPass, passInputGroup),
+          Validator.validate(repeatPassInput, Validator.checkPass, repeatPassInputGroup),
+        ]
+
+        const isValid = validationArray.every((v) => v)
+        if (!form || !isValid) {
+          return
+        }
+        const formData = new FormData(form)
+        // @ts-ignore
+        // eslint-disable-next-line no-restricted-syntax
+        for (const [name, value] of formData) {
+          console.log(`${name} = ${value}`) // key1=value1, потом key2=value2
+        }
+      },
     },
   })
   const toLoginLink = new RouterLink({
@@ -117,13 +202,13 @@ export default function renderSignUp() {
   })
   const signUpPage = new SignUpPage({
     title: 'Регистрация',
-    emailInput,
-    loginInput,
-    nameInput,
-    surnameInput,
-    phoneInput,
-    passInput,
-    repeatPassInput,
+    emailInput: emailInputGroup,
+    loginInput: loginInputGroup,
+    nameInput: nameInputGroup,
+    surnameInput: surnameInputGroup,
+    phoneInput: phoneInputGroup,
+    passInput: passInputGroup,
+    repeatPassInput: repeatPassInputGroup,
     authBtn,
     toLoginLink,
     attrs: {
